ed2f3dd8a96f86da891c0b6a4f519281d9bff38f
Julián Valenzuela <julianandresvalenzuela21@gmail.com>
2022-02-23 16:48:02 -0500

    Agregar archivos de proyecto.

---
+++ practicaPOO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace practicaPOO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
